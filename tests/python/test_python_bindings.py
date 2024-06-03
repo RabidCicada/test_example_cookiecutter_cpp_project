db90f05e96f0e25ea0a15ab43407f962a305462e
CI Test User <testuser@citestuser.com>
2024-06-03 14:17:30 +0000

    Initial Commit

---
+++ tests/python/test_python_bindings.py
@@ -0,0 +1,6 @@
+import testcookiecuttercppproject
+
+
+def test_testcookiecuttercppproject():
+    assert testcookiecuttercppproject.add_one(1) == 2
+    assert testcookiecuttercppproject.one_plus_one() == 2
